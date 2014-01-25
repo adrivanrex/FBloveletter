e174f77fe66dd330110dc2a2b1f3834c5110dcc7
adrivanrex <adrivanrex@gmail.com>
2014-01-25 23:33:06 +0800

    first

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ require 'facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => 'r',
+  'appId'  => '344617158898614',
   'secret' => '6dc8ac871858b34798bc2488200e503d',
 ));
 
