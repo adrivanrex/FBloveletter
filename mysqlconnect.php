8e067bd0ba8303bdc5d7e0548a38dea03afec12d
adrivanrex <adrivanrex@gmail.com>
2014-01-26 00:22:21 +0800

    added permission

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,3 +1,4 @@
+<?
 // Create connection
 $con=mysqli_connect("ec2-54-225-101-119.compute-1.amazonaws.com:5432","nohrdtlatgnfqc","q3X72n2i6Cllzi9t48K8o9j9Mf","db3cj65hnqhe5t");
 
@@ -6,3 +7,4 @@ if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+?>
