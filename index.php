8697178c64aab0eafa981ac4d2ad5791ab522328
adrivanrex <adrivanrex@gmail.com>
2014-01-26 00:00:06 +0800

    added html

--- index.php
+++ index.php
@@ -70,9 +70,34 @@ $naitik = $facebook->api('/naitik');
 <body>
    <div class="container">
 
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Show My Message</button>
+     <?php if ($user): ?>
+      <a href="<?php echo $logoutUrl; ?>">Logout</a>
+    <?php else: ?>
+      <div>
+        
+        <button class="btn btn-lg btn-primary btn-block" type="submit"> Show My Message</button>
+      </div>
+    <?php endif ?>
+
+    <h3>PHP Session</h3>
+    <pre><?php print_r($_SESSION); ?></pre>
+
+    <?php if ($user): ?>
+      <h3>You</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+      <h3>Your User Object (/me)</h3>
+      <pre><?php print_r($user_profile); ?></pre>
+    <?php else: ?>
+      <strong><em>You are not Connected.</em></strong>
+    <?php endif ?>
 
     </div> <!-- /container -->
+
+   
+
+    
+    
 </body>
 
 
